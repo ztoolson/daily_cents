a2b2b519a78bb5bd34be1c78b807cf6e19aff1f3
Zach Toolson <zach.toolson@mx.com>
2017-02-28 13:45:37 -0700

    setup some basic static pages that are ugly

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "DailyCents"
+  end
+
   test "should get home" do
-    get static_pages_home_url
+    get home_url
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
   end
 
+  test "should get about" do
+    get about_url
+    assert_response :success
+  end
 end
