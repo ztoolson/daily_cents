56ff1e4ab1dc27b9fe7b6ae8082ceac11bd4a9b1
Zach Toolson <zach.toolson@mx.com>
2017-02-28 16:11:35 -0700

    fix tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,11 +10,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get help" do
-    get help_url
-    assert_response :success
-  end
-
   test "should get about" do
     get about_url
     assert_response :success
